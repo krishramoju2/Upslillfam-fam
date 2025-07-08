8badb294fac0799a3bc5f62c5e47ced1c0ef9700
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-08 18:25:40 +0530

    Update route.ts

--- app/api/hrcontacts/route.ts
+++ app/api/hrcontacts/route.ts
@@ -1,4 +1,6 @@
 // app/api/hrcontacts/route.ts
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
 
 import { NextResponse } from 'next/server';
 import cheerio from 'cheerio';
