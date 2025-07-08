62f913890261d61a6846ededa4a73a2e492e2b15
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-08 18:53:43 +0530

    Update route.ts

--- app/api/hackathons/route.ts
+++ app/api/hackathons/route.ts
@@ -3,7 +3,7 @@ export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
 import { NextResponse } from 'next/server';
-import cheerio from 'cheerio';
+const cheerio = require('cheerio');
 
 export async function GET() {
   try {
