0c782d05c60a9380875e9f12200f931396dfd8c8
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-08 18:52:49 +0530

    Update route.ts

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -1,7 +1,7 @@
 // app/api/feedback/route.ts
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
-
+const cheerio = require('cheerio');
 import { NextResponse } from 'next/server';
 
 let feedbackList: { feedback: string; timestamp: string }[] = []; // In-memory storage
